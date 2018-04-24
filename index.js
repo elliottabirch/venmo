a55e33c78f2de42e5bff7a72d3708e86da8f62b1
elliottabirch <elliottabirch@gmail.com>
2018-04-23 17:42:04 -0700

    implement batching and writing to files

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const path = require('path');
 const fs = require('fs');
 
 const fileNames = fs.readdirSync(path.join(__dirname, '_SOURCE'));
+const columns = {
+  id: 'Unique Trans. ID',
+  date: 'Date',
+  note: 'Memo',
+  name: 'Name',
+  amount: 'amount',
+  fee: 'fee',
+  source: 'Account Type',
+};
 const headers = [
   'id',
   'date',
@@ -15,14 +24,22 @@ const headers = [
   'to',
   'amount',
   'fee',
-  'souce',
+  'source',
   'destination',
 ];
 
 let index = 0;
 
 hl(fileNames)
-  .flatMap(fileName => hl(fs.createReadStream(path.join(__dirname, `_OUTPUT/${fileName}-${index++}`))
-    .pipe(csvParse({ columns: headers, auto_parse: val => (!val ? undefined : val) })))
-    .drop(1));
+  .flatMap(fileName => hl(fs.createReadStream(path.join(__dirname, `_SOURCE/${fileName}`))
+    .pipe(csvParse({ columns: headers, auto_parse: val => (!val ? undefined : val) }))).drop(1))
+  .map(({ to, from, ...rest }) => Object.assign({ name: `${from}-${to}` }, rest))
+  .batch(9)
+  .stopOnError(err => console.log(err))
+  .doto(a => index++)
+  .each((rows) => {
+    console.log(rows);
+    hl(rows).pipe(csvStringify({ columns, header: true }))
+      .pipe(fs.createWriteStream(path.join(__dirname, `_OUTPUT/transactions-${index}.csv`)));
+  });
 
